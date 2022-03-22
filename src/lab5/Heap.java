215c9fc7f2b31757b4295490ef4b3895d87eb141
Parisa-022 <91896276+Parisa-022@users.noreply.github.com>
2022-03-22 23:42:11 +0800

    lab5

---
+++ src/lab5/Heap.java
@@ -0,0 +1,132 @@
+package lab5;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.lang.IllegalStateException;
+
+/**
+ * TODO class declaration
+ * We want to accept any type which is comparable to itself
+ *
+ * @param <T>
+ */
+public class Heap<T extends Comparable<T>> {
+    private ArrayList<T> container;
+
+    public Heap() {
+        container = new ArrayList<>();
+    }
+
+    /**
+     * @return If size is 0, throw {@link IllegalStateException}.
+     * Otherwise, return the first element of {@link this#container}
+     */
+    public T peek() {
+        //TODO
+    	if (this.container.size() == 0) throw new IllegalStateException();
+    	return this.container.get(0);
+    }
+
+    /**
+     *
+     * @return If size is 0, throw {@link IllegalStateException}. Otherwise, temporarily
+     *  save the first element.
+     * Afterwards, set the first position to the last element, and remove the last element.
+     * Call {@link this#heapifyDown()}, then return the original first element
+     */
+    public T poll() {
+        //TODO
+    	int size = this.container.size();
+    	if (size == 0) throw new IllegalStateException();
+    	T first = this.container.get(0);
+    	this.container.set(0, this.container.get(size-1));
+    	this.container.remove(size-1);
+    	this.heapifyDown();
+    	return first;
+    }
+
+    private void heapifyDown() {
+        int pos = 0;
+        while (hasLeft(pos)) {
+            int smallerChildIndex = getLeftIndex(pos);
+            if (hasRight(pos) && container.get(getRightIndex(pos)).compareTo(container.get(getLeftIndex(pos))) < 0) 
+            {
+                smallerChildIndex = getRightIndex(pos);
+            }
+            if (container.get(pos).compareTo(container.get(smallerChildIndex)) < 0) {
+                break;
+            } else {
+                swap(pos, smallerChildIndex);
+            }
+            pos = smallerChildIndex;
+        }
+    }
+
+    /**
+     * Add the object into {@link this#container}, then call {@link this#heapifyUp()}
+     *
+     * @param obj the object to add
+     */
+    public void add(T obj) {
+        //TODO
+    	this.container.add(obj);
+    	this.heapifyUp();
+    }
+
+    public void addAll(Collection<T> list) {
+        list.forEach(this::add);
+    }
+
+    /**
+     * While the last element has a parent and is smaller than its parent, swap the two elements. 
+     * Then, check again
+     * with the new parent until there's either no parent or we're larger than our parent.
+     */
+    private void heapifyUp() {
+        // TODO
+    	int pos = this.container.size()-1;
+    	while(this.hasParent(pos)) {
+    		int par = this.getParentIndex(pos);
+    		if (this.container.get(pos).compareTo(this.container.get(par)) < 0) {
+    			swap(pos, par);
+    			pos = par;
+    		}
+    		else {
+    			break;
+    		}
+    	}
+    }
+
+    public int size() {
+        return container.size();
+    }
+
+    private int getParentIndex(int i) {
+        return (i - 1) / 2;
+    }
+
+    private int getLeftIndex(int i) {
+        return 2 * i + 1;
+    }
+
+    private int getRightIndex(int i) {
+        return 2 * i + 2;
+    }
+
+    private boolean hasParent(int i) {
+        return getParentIndex(i) >= 0;
+    }
+
+    private boolean hasLeft(int i) {
+        return getLeftIndex(i) < container.size();
+    }
+
+    private boolean hasRight(int i) {
+        return getRightIndex(i) < container.size();
+    }
+
+    private void swap(int i1, int i2) {
+        Collections.swap(container, i1, i2);
+    }
+}
